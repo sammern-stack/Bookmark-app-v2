8fcbc1ae5e1cd7101e740b2685aa7befb354b76b
sammern <samreactdev@gmail.com>
2026-06-02 15:43:54 +0300

    feat: create custom error class handler and a custom middleware for handling errors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
 import { corsOptions } from "./config/index.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
 // Important middleware
 app.use(cors(corsOptions));
 app.use(express.json());
+
+// Roues goes here
+
+app.use(errorHandler);
 
 export default app;
