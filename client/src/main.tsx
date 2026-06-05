3d899f7e22ea9b85a5a63b64d8d181286db6b748
sammern <samreactdev@gmail.com>
2026-06-06 00:04:48 +0300

    refactor: cleaned up code for App.tsx and main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// ——— Imports —————————————————————————————————————————————————————————————————
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.scss'
+import "./index.scss";
 
-import App from './App.tsx'
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// ——— Root ————————————————————————————————————————————————————————————————————
+const root = document.getElementById("root");
+
+createRoot(root!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
