2028c5363b930bc687e019570fa4a9462e4a63d2
sammern <samreactdev@gmail.com>
2026-07-02 14:31:13 +0300

    refactor: enhance Label component with custom element rendering and enhance BookmarkMenu code with Label component

--- client/src/components/common/BookmarkList/BookmarkMenu.tsx
+++ client/src/components/common/BookmarkList/BookmarkMenu.tsx
@@ -1,5 +1,5 @@
 import { useBookmarksStore, useFormStore } from "@/stores";
-import { Icon } from "@/components/shared";
+import { Label } from "@/components/shared";
 import type { BookmarkModel } from "@/types";
 
 interface BookmarkMenuProps {
@@ -14,107 +14,94 @@ export const BookmarkMenu = ({ bookmark: b }: BookmarkMenuProps) => {
   const setUpdateFormState = useFormStore((s) => s.setUpdateFormState);
   const setSelectedBookmark = useFormStore((s) => s.setSelectedBookmark);
 
+  const handleVisit = () => increaseVisitCount(b._id);
+
+  const handleCopyToClipboard = async () => {
+    if (!navigator.clipboard) return;
+    await navigator.clipboard.writeText(b.url);
+    alert("Url copied");
+  };
+
+  const handlePin = () => {
+    const confirm = window.confirm(
+      b.pinned
+        ? "Do you want to unpinned this bookmark?"
+        : "Do you want to pin this bookmark?",
+    );
+    if (confirm) updatePinned(b._id);
+  };
+
+  const handleArchive = () => {
+    const confirm = window.confirm(
+      b.isArchived
+        ? "Do you want to move this bookmark to the active list?"
+        : "Do you want to archive this bookmark?",
+    );
+    if (confirm) updateIsArchived(b._id);
+  };
+
+  const handleEdit = () => {
+    setSelectedBookmark(b);
+    setUpdateFormState("open");
+  };
+
+  const handleDelete = () => {
+    const confirm = window.confirm("Do you want to delete this bookmark?");
+    if (confirm) deleteBookmark(b._id);
+  };
+
   return (
     <>
-      <a
+      <Label
+        as="a"
         className="bookmark__menu-item"
         href={b.url}
         target="_blank"
         rel="noopener noreferrer"
-        onClick={() => increaseVisitCount(b._id)}
-      >
-        <Icon name="icon-visit" />
-        Visit
-      </a>
+        onClick={handleVisit}
+        label="Visit"
+        icon="icon-visit"
+      />
 
-      <div
+      <Label
         className="bookmark__menu-item"
-        onClick={async () => {
-          if (!navigator.clipboard) return;
-          await navigator.clipboard.writeText(b.url);
-          alert("Url copied");
-        }}
-      >
-        <Icon name="icon-copy" />
-        Copy URL
-      </div>
+        onClick={handleCopyToClipboard}
+        label="Copy URL"
+        icon="icon-copy"
+      />
 
       {!b.isArchived && (
         <>
-          <div
+          <Label
             className="bookmark__menu-item"
-            onClick={() => {
-              const confirm = window.confirm(
-                b.pinned
-                  ? "Do you want to unpinned this bookmark?"
-                  : "Do you want to pin this bookmark?",
-              );
-              if (confirm) updatePinned(b._id);
-            }}
-          >
-            {b.pinned ? (
-              <>
-                <Icon name="icon-unpin" />
-                Unpin
-              </>
-            ) : (
-              <>
-                <Icon name="icon-pin" />
-                Pin
-              </>
-            )}
-          </div>
+            onClick={handlePin}
+            label={b.pinned ? "Unpin" : "Pin"}
+            icon={`icon-${b.pinned ? "unpin" : "pin"}`}
+          />
 
-          <div
+          <Label
             className="bookmark__menu-item"
-            onClick={() => {
-              setSelectedBookmark(b);
-              setUpdateFormState("open");
-            }}
-          >
-            <Icon name="icon-edit" />
-            Edit
-          </div>
+            onClick={handleEdit}
+            label="Edit"
+            icon="icon-edit"
+          />
         </>
       )}
 
-      <div
+      <Label
         className="bookmark__menu-item"
-        onClick={() => {
-          const confirm = window.confirm(
-            b.isArchived
-              ? "Do you want to move this bookmark to the active list?"
-              : "Do you want to archive this bookmark?",
-          );
-          if (confirm) updateIsArchived(b._id);
-        }}
-      >
-        {b.isArchived ? (
-          <>
-            <Icon name="icon-unarchive" />
-            Unarchive
-          </>
-        ) : (
-          <>
-            <Icon name="icon-archive" />
-            Archive
-          </>
-        )}
-      </div>
+        onClick={handleArchive}
+        label={b.isArchived ? "Unarchive" : "Archive"}
+        icon={`icon-${b.isArchived ? "unarchive" : "archive"}`}
+      />
 
       {b.isArchived && (
-        <div
+        <Label
           className="bookmark__menu-item"
-          onClick={() => {
-            const confirm = window.confirm(
-              "Do you want to delete this bookmark?",
-            );
-            if (confirm) deleteBookmark(b._id);
-          }}
-        >
-          <Icon name="icon-delete" />
-          Delete Permanently
-        </div>
+          onClick={handleDelete}
+          label="Delete Permanently"
+          icon="icon-delete"
+        />
       )}
     </>
   );
--- client/src/components/shared/Label.tsx
+++ client/src/components/shared/Label.tsx
@@ -1,16 +1,29 @@
+import type { ComponentPropsWithoutRef, ElementType } from "react";
 import { Icon } from "./Icon";
 
-type LabelProps = {
+type LabelProps<T extends ElementType> = {
+  as?: T;
   className?: string;
   label: string | number;
   icon?: string;
-};
+} & ComponentPropsWithoutRef<T>;
 
-export const Label = ({ className, label, icon }: LabelProps) => {
+// type LabelProps<T extends ElementType> = BaseProps<T> &
+//   Omit<ComponentPropsWithoutRef<T>, keyof BaseProps<T>>;
+
+export const Label = <T extends ElementType>({
+  as,
+  className,
+  label,
+  icon,
+  ...props
+}: LabelProps<T>) => {
+  const Component = as || "div";
+
   return (
-    <div className={className}>
+    <Component className={className} {...props}>
       {icon && <Icon name={icon} />}
       {label}
-    </div>
+    </Component>
   );
 };
