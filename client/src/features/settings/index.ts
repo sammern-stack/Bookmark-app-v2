63b4041a38e64896a262896ce8b8be5eeefb3328
sammern <samreactdev@gmail.com>
2026-07-12 16:33:14 +0300

    fix: correct import path for SortbyOption component

--- client/src/features/settings/index.ts
+++ client/src/features/settings/index.ts
@@ -4,4 +4,4 @@ export { TagFilter } from "./components/TagFilter/TagFilter";
 export { Searchbar } from "./components/Searchbar/Searchbar";
 export { ThemeSwitch } from "./components/ThemeSwitch/ThemeSwitch";
 export { SortbyButton } from "./components/SortbyButton/SortbyButton";
-export { SortbyOption } from "./components/SortbyOption/SortByOption";
+export { SortbyOption } from "./components/SortbyOption/SortbyOption";
