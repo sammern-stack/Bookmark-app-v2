6d25b14fbf6a00d4e0c922d2e6b3e6717858198c
sammern <samreactdev@gmail.com>
2026-09-23 20:15:06 +0300

    refactor: replace hardcoded staleTime with constants for improved readability

--- client/src/config/queryClient.config.ts
+++ client/src/config/queryClient.config.ts
@@ -1,11 +1,14 @@
 import type { QueryClientConfig } from "@tanstack/react-query";
 
+const STALE_TIME_MINS = 5;
+const STALE_TIME_MILLISECONDS = STALE_TIME_MINS * 60 * 1000;
+
 export const queryClientConfig: QueryClientConfig = {
   defaultOptions: {
     queries: {
       retry: 1,
       refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 5,
+      staleTime: STALE_TIME_MILLISECONDS,
     },
   },
 };
