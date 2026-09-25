e99732799db6ec2279f3b515942463bbf6a285cd
sammern <samreactdev@gmail.com>
2026-09-25 10:22:20 +0300

    refactor: remove unused hamburger menu component; enhance PageLayout with toggle functionality for sidebar

--- client/src/layout/PageHeader/PageHeader.tsx
+++ client/src/layout/PageHeader/PageHeader.tsx
@@ -1,35 +1,24 @@
 import styles from "./PageHeader.module.scss";
 import { ThemeSwitch } from "@/features/settings";
 import { ProfileIcon, ProfileInfo, LogoutButton } from "@/features/profile";
-import { useUIVisibilityStore, useDialogStore } from "@/shared/stores";
+import { useDialogStore } from "@/shared/stores";
 import { Button } from "@/shared/components";
 import { useDropdown } from "@/shared/hooks";
 
-import HamburgerIcon from "@/assets/images/icon-menu-hamburger.svg?react";
 import PlusIcon from "@/assets/images/icon-add.svg?react";
 import SearchIcon from "@/assets/images/icon-search.svg?react";
 
 export const PageHeader = () => {
   const { dropdownRef, openDropdown, toggle } = useDropdown();
-  const { toggle: toggleSidebar } = useUIVisibilityStore.getState();
   const { openDialog } = useDialogStore.getState();
-
-  // TODO: Remove it when adding responsiveness
-  const isMobile = false;
 
   const handleOpenCreateBookmarkForm = () => {
     openDialog({ type: "createBookmark", payload: null });
   };
-  const handleToggleSidebar = () => toggleSidebar("bookmarkSidebar");
-
+  
   return (
     <>
       <div className={styles.pageHeader__section}>
-        {isMobile && (
-          <Button variant="secondary" onClick={handleToggleSidebar}>
-            <HamburgerIcon />
-          </Button>
-        )}
         <div className={styles.pageHeader__search}>
           <SearchIcon />
           <input type="text" placeholder="Search by title..." />
--- client/src/layout/PageLayout/PageLayout.tsx
+++ client/src/layout/PageLayout/PageLayout.tsx
@@ -1,5 +1,10 @@
-import type { PropsWithChildren } from "react";
 import styles from "./PageLayout.module.scss";
+import type { PropsWithChildren } from "react";
+import { Button } from "@/shared/components";
+
+import CloseIcon from "@/assets/images/icon-close.svg?react";
+import HamburgerIcon from "@/assets/images/icon-menu-hamburger.svg?react";
+import { useToggle } from "@/shared/hooks";
 
 interface PageLayoutProps extends PropsWithChildren {
   header?: React.ReactNode;
@@ -7,10 +12,31 @@ interface PageLayoutProps extends PropsWithChildren {
 }
 
 export const PageLayout = ({ children, header, sidebar }: PageLayoutProps) => {
+  const [aside, toggleAside] = useToggle();
+
   return (
     <div className={styles.layout}>
-      <header className={styles.layout__header}>{header}</header>
-      <aside className={styles.layout__sidebar}>{sidebar}</aside>
+      <header className={styles.layout__header}>
+        <Button
+          variant="secondary"
+          className={styles.layout__hamburgerMenu}
+          onClick={toggleAside}
+        >
+          <HamburgerIcon />
+        </Button>
+        <div className={styles.layout__headerContent}>{header}</div>
+      </header>
+      <aside
+        className={[
+          styles.layout__sidebar,
+          aside ? styles["layout__sidebar--open"] : "",
+        ].join(" ")}
+      >
+        <button className={styles.layout__sidebarClose} onClick={toggleAside}>
+          <CloseIcon />
+        </button>
+        {sidebar}
+      </aside>
       <main className={styles.layout__main}>{children}</main>
     </div>
   );
